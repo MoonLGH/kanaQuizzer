97fc960a23ce618e1dc35dd793926fce3ec5b95b
MoonLGH <MoonLMC1212@gmail.com>
2022-10-21 13:49:07 +0000

    kata sifat lets goo

---
+++ assets/Javascripts/katasifat.js
@@ -0,0 +1,606 @@
+let arr = [
+    {
+        "jepang": "べんり",
+        "romaji": "benri",
+        "indonesia": "praktis"
+    },
+    {
+        "jepang": "だいじょうぶ",
+        "romaji": "daijoubu",
+        "indonesia": "baik-baik saja"
+    },
+    {
+        "jepang": "大きらい",
+        "romaji": "daikirai",
+        "indonesia": "sangat benci"
+    },
+    {
+        "jepang": "大好き",
+        "romaji": "daisuki",
+        "indonesia": "sangat suka"
+    },
+    {
+        "jepang": "だめ",
+        "romaji": "dame",
+        "indonesia": "tidak boleh, tidak bagus"
+    },
+    {
+        "jepang": "ふべん",
+        "romaji": "fuben",
+        "indonesia": "ribet, tidak praktis"
+    },
+    {
+        "jepang": "元気",
+        "romaji": "genki",
+        "indonesia": "sehat"
+    },
+    {
+        "jepang": "ハンサム",
+        "romaji": "hansamu",
+        "indonesia": "tanpan"
+    },
+    {
+        "jepang": "下手",
+        "romaji": "heta",
+        "indonesia": "payah"
+    },
+    {
+        "jepang": "ひま",
+        "romaji": "hima",
+        "indonesia": "senggang"
+    },
+    {
+        "jepang": "ひつよう",
+        "romaji": "hitsuyou",
+        "indonesia": "perlu"
+    },
+    {
+        "jepang": "いろいろ",
+        "romaji": "iroiro",
+        "indonesia": "macam-macam"
+    },
+    {
+        "jepang": "いっしょうけんめい",
+        "romaji": "isshoukenmei",
+        "indonesia": "sungguh-sungguh"
+    },
+    {
+        "jepang": "いや",
+        "romaji": "iya",
+        "indonesia": "tidak mengenakan"
+    },
+    {
+        "jepang": "じゆう",
+        "romaji": "jiyuu",
+        "indonesia": "bebas"
+    },
+    {
+        "jepang": "じょうぶ",
+        "romaji": "joubu",
+        "indonesia": "kuat"
+    },
+    {
+        "jepang": "上手",
+        "romaji": "jouzu",
+        "indonesia": "mahir, pandai"
+    },
+    {
+        "jepang": "じゅうぶん",
+        "romaji": "juubun",
+        "indonesia": "cukup"
+    },
+    {
+        "jepang": "かんたん",
+        "romaji": "kantan",
+        "indonesia": "mudah"
+    },
+    {
+        "jepang": "けっこう",
+        "romaji": "kekkou",
+        "indonesia": "lumayan baik, lumayan bagus"
+    },
+    {
+        "jepang": "きけん",
+        "romaji": "kiken",
+        "indonesia": "berbahaya"
+    },
+    {
+        "jepang": "きらい",
+        "romaji": "kirai",
+        "indonesia": "benci"
+    },
+    {
+        "jepang": "きれい",
+        "romaji": "kirei",
+        "indonesia": "indah/bersih"
+    },
+    {
+        "jepang": "まじめ",
+        "romaji": "majime",
+        "indonesia": "serius"
+    },
+    {
+        "jepang": "まっすぐ",
+        "romaji": "massugu",
+        "indonesia": "lurus"
+    },
+    {
+        "jepang": "みどり",
+        "romaji": "midori",
+        "indonesia": "hijau"
+    },
+    {
+        "jepang": "むらさき",
+        "romaji": "murasaki",
+        "indonesia": "ungu"
+    },
+    {
+        "jepang": "ねっしん",
+        "romaji": "nesshin",
+        "indonesia": "antusias"
+    },
+    {
+        "jepang": "にぎやか",
+        "romaji": "nigiyaka",
+        "indonesia": "ramai"
+    },
+    {
+        "jepang": "らく",
+        "romaji": "raku",
+        "indonesia": "nyaman"
+    },
+    {
+        "jepang": "りっぱ",
+        "romaji": "rippa",
+        "indonesia": "megah, terpuji, terkemuka"
+    },
+    {
+        "jepang": "しんせつ",
+        "romaji": "shinsetsu",
+        "indonesia": "ramah"
+    },
+    {
+        "jepang": "しんぱい",
+        "romaji": "shinpai",
+        "indonesia": "mengkhawatirkan"
+    },
+    {
+        "jepang": "しずか",
+        "romaji": "shizuka",
+        "indonesia": "hening"
+    },
+    {
+        "jepang": "好き",
+        "romaji": "suki",
+        "indonesia": "suka"
+    },
+    {
+        "jepang": "たいへん",
+        "romaji": "taihen",
+        "indonesia": "gawat"
+    },
+    {
+        "jepang": "大切",
+        "romaji": "taisetsu",
+        "indonesia": "penting / berharga"
+    },
+    {
+        "jepang": "ていねい",
+        "romaji": "teinei",
+        "indonesia": "sopan"
+    },
+    {
+        "jepang": "てきとう",
+        "romaji": "tekitou",
+        "indonesia": "cocok"
+    },
+    {
+        "jepang": "とくべつ",
+        "romaji": "tokubetsu",
+        "indonesia": "spesial"
+    },
+    {
+        "jepang": "ゆうめい",
+        "romaji": "yuumei",
+        "indonesia": "terkenal"
+    },
+    {
+        "jepang": "ざんねん",
+        "romaji": "zannen",
+        "indonesia": "mengecewakan, menyayangkan"
+    },
+    {
+        "jepang": "あぶない",
+        "romaji": "abunai",
+        "indonesia": "bahaya"
+    },
+    {
+        "jepang": "あかい",
+        "romaji": "akai",
+        "indonesia": "merah"
+    },
+    {
+        "jepang": "あかるい",
+        "romaji": "akarui",
+        "indonesia": "terang"
+    },
+    {
+        "jepang": "あまい",
+        "romaji": "amai",
+        "indonesia": "manis"
+    },
+    {
+        "jepang": "あおい",
+        "romaji": "aoi",
+        "indonesia": "biru"
+    },
+    {
+        "jepang": "新しい",
+        "romaji": "atarashii",
+        "indonesia": "baru"
+    },
+    {
+        "jepang": "あたたかい",
+        "romaji": "atatakai",
+        "indonesia": "hangat"
+    },
+    {
+        "jepang": "あつい",
+        "romaji": "atsui",
+        "indonesia": "panas, tebal"
+    },
+    {
+        "jepang": "ちゃいろい",
+        "romaji": "chairoi",
+        "indonesia": "warna coklat"
+    },
+    {
+        "jepang": "小さい",
+        "romaji": "chiisai",
+        "indonesia": "kecil"
+    },
+    {
+        "jepang": "ちかい",
+        "romaji": "chikai",
+        "indonesia": "dekat"
+    },
+    {
+        "jepang": "ふかい",
+        "romaji": "fukai",
+        "indonesia": "dalam"
+    },
+    {
+        "jepang": "ふるい",
+        "romaji": "furui",
+        "indonesia": "tua, lama"
+    },
+    {
+        "jepang": "ふとい",
+        "romaji": "futoi",
+        "indonesia": "gemuk"
+    },
+    {
+        "jepang": "はやい",
+        "romaji": "hayai",
+        "indonesia": "cepat"
+    },
+    {
+        "jepang": "ひくい",
+        "romaji": "hikui",
+        "indonesia": "rendah"
+    },
+    {
+        "jepang": "広い",
+        "romaji": "hiroi",
+        "indonesia": "luas"
+    },
+    {
+        "jepang": "ほしい",
+        "romaji": "hoshii",
+        "indonesia": "ingin"
+    },
+    {
+        "jepang": "ほそい",
+        "romaji": "hosoi",
+        "indonesia": "tipis, ramping"
+    },
+    {
+        "jepang": "いそがしい",
+        "romaji": "isogashii",
+        "indonesia": "sibuk"
+    },
+    {
+        "jepang": "いたい",
+        "romaji": "itai",
+        "indonesia": "sakit"
+    },
+    {
+        "jepang": "からい",
+        "romaji": "karai",
+        "indonesia": "pedas"
+    },
+    {
+        "jepang": "かるい",
+        "romaji": "karui",
+        "indonesia": "ringan"
+    },
+    {
+        "jepang": "かわいい",
+        "romaji": "kawaii",
+        "indonesia": "lucu"
+    },
+    {
+        "jepang": "きいろい",
+        "romaji": "kiiroi",
+        "indonesia": "kuning"
+    },
+    {
+        "jepang": "きたない",
+        "romaji": "kitanai",
+        "indonesia": "kotor"
+    },
+    {
+        "jepang": "くらい",
+        "romaji": "kurai",
+        "indonesia": "gelap"
+    },
+    {
+        "jepang": "くろい",
+        "romaji": "kuroi",
+        "indonesia": "hitam"
+    },
+    {
+        "jepang": "まるい",
+        "romaji": "marui",
+        "indonesia": "bulat, bundar"
+    },
+    {
+        "jepang": "まずい",
+        "romaji": "mazui",
+        "indonesia": "tidak enak"
+    },
+    {
+        "jepang": "みじかい",
+        "romaji": "mijikai",
+        "indonesia": "pendek"
+    },
+    {
+        "jepang": "むずかしい",
+        "romaji": "muzukashii",
+        "indonesia": "sulit"
+    },
+    {
+        "jepang": "長い",
+        "romaji": "nagai",
+        "indonesia": "panjang"
+    },
+    {
+        "jepang": "ない",
+        "romaji": "nai",
+        "indonesia": "tidak ada"
+    },
+    {
+        "jepang": "にがい",
+        "romaji": "nigai",
+        "indonesia": "pahit"
+    },
+    {
+        "jepang": "おいしい",
+        "romaji": "oishii",
+        "indonesia": "enak"
+    },
+    {
+        "jepang": "おもい",
+        "romaji": "omoi",
+        "indonesia": "berat"
+    },
+    {
+        "jepang": "おもしろい",
+        "romaji": "omoshiroi",
+        "indonesia": "menarik"
+    },
+    {
+        "jepang": "おおい",
+        "romaji": "ooi",
+        "indonesia": "banyak"
+    },
+    {
+        "jepang": "大きい",
+        "romaji": "ookii",
+        "indonesia": "besar"
+    },
+    {
+        "jepang": "おそい",
+        "romaji": "osoi",
+        "indonesia": "lambat"
+    },
+    {
+        "jepang": "さむい",
+        "romaji": "samui",
+        "indonesia": "dingin (cuaca)"
+    },
+    {
+        "jepang": "せまい",
+        "romaji": "semai",
+        "indonesia": "sempit"
+    },
+    {
+        "jepang": "白い",
+        "romaji": "shiroi",
+        "indonesia": "putih"
+    },
+    {
+        "jepang": "すくない",
+        "romaji": "sukunai",
+        "indonesia": "sedikit"
+    },
+    {
+        "jepang": "すごい",
+        "romaji": "sugoi",
+        "indonesia": "hebat"
+    },
+    {
+        "jepang": "するどい",
+        "romaji": "surudoi",
+        "indonesia": "tajam"
+    },
+    {
+        "jepang": "すずしい",
+        "romaji": "suzushii",
+        "indonesia": "sejuk"
+    },
+    {
+        "jepang": "高い",
+        "romaji": "takai",
+        "indonesia": "tinggi/mahal"
+    },
+    {
+        "jepang": "たのしい",
+        "romaji": "tanoshii",
+        "indonesia": "menyenangkan"
+    },
+    {
+        "jepang": "とおい",
+        "romaji": "tooi",
+        "indonesia": "jauh"
+    },
+    {
+        "jepang": "つまらない",
+        "romaji": "tsumaranai",
+        "indonesia": "membosankan"
+    },
+    {
+        "jepang": "つめたい",
+        "romaji": "tsumetai",
+        "indonesia": "dingin (benda)"
+    },
+    {
+        "jepang": "強い",
+        "romaji": "tsuyoi",
+        "indonesia": "kuat"
+    },
+    {
+        "jepang": "うれしい",
+        "romaji": "ureshii",
+        "indonesia": "senang"
+    },
+    {
+        "jepang": "うるさい",
+        "romaji": "urusai",
+        "indonesia": "berisik"
+    },
+    {
+        "jepang": "うすい",
+        "romaji": "usui",
+        "indonesia": "tipis"
+    },
+    {
+        "jepang": "わかい",
+        "romaji": "wakai",
+        "indonesia": "muda"
+    },
+    {
+        "jepang": "やさしい",
+        "romaji": "yasashii",
+        "indonesia": "lembut"
+    },
+    {
+        "jepang": "安い",
+        "romaji": "yasui",
+        "indonesia": "murah"
+    },
+    {
+        "jepang": "よい/いい",
+        "romaji": "yoi/ii",
+        "indonesia": "bagus"
+    }
+]
+
+window.onload = function () {
+    let q = GenerateNewQuestion()
+}
+
+
+const inputAnswer = document.querySelector("#answer > input")
+const buttonAnswer = document.querySelector("#answer button")
+const QuestionBox = document.querySelector("#question > h3")
+
+
+function GenerateNewQuestion() {
+    let q = arr[Math.floor(Math.random() * arr.length)]
+
+    let phase = localStorage.getItem("phase")
+    if(phase === "Relearn letter"){
+        let words = JSON.parse(localStorage.getItem("sifatData"))
+        words.sort((a,b) => a.counter-b.counter)
+        console.log("RELEARN    ")
+        console.log(words)
+        if(!words.every((val, i, words) => val.counter === words[0].counter)){
+            putQuestion(words[0].letter)
+        }else{
+            makeAlert("Seems like you understand these word, Randomizing","alert-success")
+            setPhase("Generating Random Letter","10")
+            putQuestion(q)
+        }
+    }else{
+        putQuestion(q)
+    }
+}
+
+function putQuestion(question) {
+    // put q.jepang to question box
+    console.log(question)
+    if(question.jepang){
+        QuestionBox.innerHTML = question.jepang
+    } else {
+        QuestionBox.innerHTML = question
+    }
+}
+
+buttonAnswer.addEventListener("click", (e) => {
+    e.preventDefault()
+    answer()
+    inputAnswer.value = ""
+})
+
+inputAnswer.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        answer()
+        inputAnswer.value = ""
+    }
+})
+
+function answer() {
+    // get answer from arr.find questionBox
+    let q = arr.find((q) => q.jepang == QuestionBox.innerHTML)
+    // check answer
+    if (q.romaji == inputAnswer.value) {
+        makeAlert(`Correct ${q.jepang} in romaji is ${q.romaji} Meaning:${q.indonesia}`, "alert-success")
+        correct(q.jepang)
+    }
+    else {
+        makeAlert(`Wrong ${q.jepang} in romaji is ${q.romaji} Meaning:${q.indonesia}`, "alert-danger")
+        wrong(q.jepang)
+    }
+    let newQ = GenerateNewQuestion()
+    inputAnswer.value = ""
+}
+
+
+
+let alert = document.querySelector("#alerts")
+function makeAlert(text, mode) {
+    let newalert = document.createElement("div")
+    newalert.classList.add("alert")
+    newalert.classList.add(mode)
+    newalert.innerText = text
+    alert.appendChild(newalert)
+    setTimeout(() => {
+        removeAlert(newalert)
+    }, 3000)
+}
+
+function removeAlert(alertbox) {
+    alert.removeChild(alertbox)
+}
---
+++ assets/Javascripts/katasifatLogic.js
@@ -0,0 +1,127 @@
+function initialcheck() {
+    let phase = localStorage.getItem("phase")
+    let number = localStorage.getItem("number")
+    let sifatLearned = localStorage.getItem("sifatLearned")
+    let sifatData = localStorage.getItem("sifatData")
+    let kataSifatData = localStorage.getItem("kataSifatData")
+    let kataData = localStorage.getItem("katakanaCounter")
+
+    if (!phase) {
+        localStorage.setItem("phase", "Generating Random Letter")
+    }
+    if (!number) {
+        localStorage.setItem("number", "10")
+    }
+    if (!sifatLearned) {
+        localStorage.setItem("sifatLearned", "[]")
+    }
+    if (!sifatData) {
+        localStorage.setItem("sifatData", "[]")
+    }
+    if (!kataData) {
+        localStorage.setItem("katakanaCounter", '{"correct":0,"wrong":0}')
+    }
+    if (!kataSifatData) {
+        localStorage.setItem("kataSifatCounter", '{"correct":0,"wrong":0}')
+    }
+    updateInfo()
+}
+initialcheck()
+
+function updateInfo(){
+    let katacount = JSON.parse(localStorage.getItem("kataSifatCounter"))
+    document.querySelector("#kataI > .correct").innerText = `Correct : ${katacount.correct}`
+    document.querySelector("#kataI > .wrong").innerText = `Wrong : ${katacount.wrong}`
+
+    updatePhase(true)
+    let phase = localStorage.getItem("phase")
+    let nextPhase = localStorage.getItem("number")
+    document.querySelector("#stageI > .phase").innerText = `Phase : ${phase}`
+    document.querySelector("#stageI > .nextPhase").innerText = `NextPhase in : ${nextPhase}`
+}
+
+function correct(Letter) {
+    updateScore(Letter,"Correct")
+    count(Letter, "Correct")
+}
+
+function wrong(Letter) {
+    updateScore(Letter,"Wrong")
+    count(Letter, "Wrong")
+}
+
+function count(Letter, type) {
+    let parselearned = JSON.parse(localStorage.getItem("sifatLearned"))
+    if (!parselearned.includes(Letter)) {
+        parselearned.push(Letter)
+        localStorage.setItem("sifatLearned", JSON.stringify(parselearned))
+    }
+
+    newData = JSON.parse(localStorage.getItem("sifatData"))
+    let data = newData.find(item => item.letter === Letter)
+
+    if (!data) {
+        data = {
+            letter: Letter,
+            counter: 0
+        }
+        newData.push(data)
+    }
+
+    let counter = data.counter
+    if (type === "Correct") {
+        counter++
+    } else if (type === "Wrong") {
+        counter--
+    }
+
+    data.counter = counter
+    localStorage.setItem("sifatData", JSON.stringify(newData))
+}
+
+function updateScore(Letter,type){
+        let hiraData = JSON.parse(localStorage.getItem("kataSifatCounter"))
+        if(type === "Correct"){
+            hiraData.correct++
+        }else if(type === "Wrong"){
+            hiraData.wrong++
+        }
+        localStorage.setItem("kataSifatCounter", JSON.stringify(hiraData))
+    updatePhase()
+    updateInfo()
+}
+
+function updatePhase(initial){
+    let phase = localStorage.getItem("phase")
+    let number = localStorage.getItem("number")
+
+    if(initial){
+        return
+    }
+    if(phase === "Generating Random Letter"){
+        if(number === "0"){
+            localStorage.setItem("phase", "Relearn letter")
+            localStorage.setItem("number", "10")
+        }
+    }
+
+    if(phase === "Relearn letter"){
+        if(number === "0"){
+            localStorage.setItem("phase", "Generating Random Letter")
+            localStorage.setItem("number", "10")
+        }
+    }
+
+    if(number - 1 < 0){
+        localStorage.setItem("number", "10")
+    }
+    else{
+        localStorage.setItem("number", number - 1)
+    }
+}
+
+function setPhase(phase,counter){
+    localStorage.setItem("phase", phase)
+    localStorage.setItem("number", counter)
+    updateInfo()
+}
